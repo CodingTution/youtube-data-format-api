f01ad81f35ec59f7d9918bf9cfd61ec0425d4422
CodingTution <77720398+CodingTution@users.noreply.github.com>
2021-01-21 02:35:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const httprequest = require('request');
 const videoUrlLink = require('video-url-link');
+var responsearray = [];
+var array = [];
 var port = process.env.PORT || 3000
 var express = require('express')
 var app = express()
-var responsearray = [];
-var array = [];
+
 app.get('/', function (request, response) {
-    response.send("hello");
+    var urlmain = __dirname + request.url;
+    var query = gup('query', urlmain);
+    var key = gup('key', urlmain);
+    var maxResults = gup('maxResults', urlmain);
+    httprequest(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${query}&key=${key}`, function (error, res, body) {
+        var responsebody = JSON.parse(body).items;
+        if (responsebody != undefined) {
+            responsebody.map((item, index) => {
+                array.push(item.id.videoId);
+                if (array.length == 10) {
+                    array.map((item, index) => {
+                        videoUrlLink.youtube.getInfo(`https://youtu.be/${item}`, { hl: 'en' }, (error, info) => {
+                            responsearray.push({
+                                details: info.details,
+                                formats: info.formats,
+                            })
+                            if (responsearray.length == 10) {
+                                response.send(JSON.stringify(responsearray))
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    });
 })
 
 app.listen(port)
