876902a14d1a3463ee4428ae46487f76bca6f9f6
CodingTution <77720398+CodingTution@users.noreply.github.com>
2021-01-22 08:03:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-const httprequest = require('request');
-const videoUrlLink = require('video-url-link');
+const httprequest = require("request");
+const videoUrlLink = require("video-url-link");
 var responsearray = [];
 var array = [];
-var port = process.env.PORT || 3000
-var express = require('express')
-var app = express()
+var port = process.env.PORT || 3000;
+var express = require("express");
+var app = express();
 
-app.get('/', function (request, response) {
-    var urlmain = __dirname + request.url;
-    var query = gup('query', urlmain);
-    var key = gup('key', urlmain);
-    var maxResults = gup('maxResults', urlmain);
-    httprequest(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${query}&key=${key}`, function (error, res, body) {
-        var responsebody = JSON.parse(body).items;
-        if (responsebody != undefined) {
-            responsebody.map((item, index) => {
-                array.push(item.id.videoId);
-                if (array.length == 10) {
-                    array.map((item, index) => {
-                        videoUrlLink.youtube.getInfo(`https://youtu.be/${item}`, { hl: 'en' }, (error, info) => {
-                            responsearray.push({
-                                details: info.details,
-                                formats: info.formats,
-                            })
-                            if (responsearray.length == 10) {
-                                response.setHeader("Content-Type", "application/json");
-                                response.json(responsearray);
-                            }
-                        });
-                    });
+app.get("/", function (request, response) {
+  var urlmain = __dirname + request.url;
+  var query = gup("query", urlmain);
+  var key = gup("key", urlmain);
+  var maxResults = gup("maxResults", urlmain);
+  httprequest(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${query}&key=${key}`,
+    function (error, res, body) {
+      var responsebody = JSON.parse(body).items;
+      if (responsebody != undefined) {
+        responsebody.map((item, index) => {
+          array.push(item.id.videoId);
+          if (array.length == 10) {
+            array.map((item, index) => {
+              videoUrlLink.youtube.getInfo(
+                `https://youtu.be/${item}`,
+                { hl: "en" },
+                (error, info) => {
+                  responsearray.push({
+                    details: info.details,
+                    formats: info.formats
+                  });
+                  if (responsearray.length == 10) {
+                    response.setHeader("Content-Type", "application/json");
+                    response.json(responsearray);
+                    responsearray = [];
+                    array = [];
+                  }
                 }
+              );
             });
-        }
-    });
-})
+          }
+        });
+      }
+    }
+  );
+});
 
-app.listen(port)
+app.listen(port);
 function gup(name, url) {
-    if (!url) url = location.href;
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\?&]" + name + "=([^&#]*)";
-    var regex = new RegExp(regexS);
-    var results = regex.exec(url);
-    return results == null ? null : results[1];
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(url);
+  return results == null ? null : results[1];
 }
