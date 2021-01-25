25d1a7ff12a4f40e8ee53e1e87a24531e5a64aa9
CodingTution <77720398+CodingTution@users.noreply.github.com>
2021-01-25 03:02:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var array = [];
-var port = process.env.PORT || 3000;
+var port = 8080;
 var express = require("express");
-const videoUrlLink = require('video-url-link');
 var app = express();
+const https = require('https');
 app.get("/", function (request, response) {
     var urlmain = __dirname + request.url;
     var id = gup("id", urlmain);
-    videoUrlLink.youtube.getInfo('https://youtu.be/' + id, { hl: 'en' }, (error, info) => {
-        if (error) {
-            console.error(error);
-        } else {
-            response.send(info.formats);
-        }
+    https.get(`https://www.yt-download.org/api/button/mp3/${id}`, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+        res.setEncoding("utf-8");
+        res.on('data', (body) => {
+            var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+            return body.replace(urlRegex, function (url) {
+                array.push(url)
+                if (array.length == 7) {
+                    response.send(array[3]);
+                    array = [];
+                }
+                return '<a href="' + url + '">' + url + '</a>';
+            });
+        });
+
+    }).on('error', (e) => {
+        console.error(e);
     });
 });
 
