5ae17dbc4e66a7f6731f41fa971dcac74c856134
CodingTution <77720398+CodingTution@users.noreply.github.com>
2021-03-29 01:31:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,62 @@ io.on('connection', (socket) => {
     io.emit("getuser", "nothing");
   });
 });
-
-http.listen(process.env.PORT, () => {
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var request = require('request');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+var users = [];
+io.on('connection', (socket) => {
+  io.emit("getuser", "nothing");
+  socket.on('newuser', (id) => {
+    request.get(
+      'http://localhost/chatonly/php/setstatus.php?id=' + id + "&status=online",
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+        }
+      }
+    );
+    var obj = {
+      name: id,
+      id: socket.id
+    }
+    var x = true;
+    users.map((item, index) => {
+      if (item.name == id) {
+        x = false;
+      }
+    });
+    if (x) {
+      users.push(obj);
+    }
+    socket.on('disconnect', function () {
+      io.emit("getuser", "nothing");
+      request.get(
+        'http://localhost/chatonly/php/setstatus.php?id=' + id + "&status=offline",
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+          }
+        }
+      );
+    });
+  });
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+    io.emit("getuser", "nothing");
+  });
+});
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+http.listen(3000, () => {
   console.log('listening on *:3000');
+});
+http.listen(process.env.PORT, () => {
+  console.log('listening on : ' + process.env.PORT);
 });
